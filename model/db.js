8217817b8647b8675aac70ed0af8dd68bc4ca0ee
Harold Shinsato <harold@shinsato.com>
2015-10-17 17:47:20 -0600

    adding mongodb-uri

--- model/db.js
+++ model/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-
+var uriUtil = require('mongodb-uri');
 
 var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
                 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };  
